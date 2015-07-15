3c5bb1f51321c55f552871b5f33086c95f5fbe7e
Jose Mugaburu <jose.mugaburu@gmail.com>
2015-07-14 22:55:54 -0400

    adds init file and first inversion try

---
+++ Algo I - Stanford/inversions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+    Read the input from an array/text and return the number of inversions.
+"""
+
+def inversion():
+
+
+    return None
--- Intro to CS and Algo - MIT/__init__.py
+++ Intro to CS and Algo - MIT/__init__.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python
+
+"""
+ This project includes my training and version on Python and data structures
+  along with simple exercises from the Intro to CS from MIT in edX
+"""
+
+
 __author__ = 'jmugz3'
+__maintainer__ = "jmugz3"
+__email__ = "mugab686@gmail.edu"
+__status__ = "Prototype"
+
