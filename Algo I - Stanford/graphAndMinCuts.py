7a40a22e41e8c821e1ef458bbe86278cd4e6d051
Jose Mugaburu <jose.mugaburu@gmail.com>
2015-08-01 21:27:41 -0400

    adds test file and initial structure for undirected graph implementation

---
+++ Algo I - Stanford/graphAndMinCuts.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+    Reads the input from an array/text and return the number of inversions.
+    let i, j => array indices
+    An inversion is number of pairs (i,j) where i < j
+"""
+
+from main import readInput
+
+def countMinCuts():
+    count = 0
+
+    return count
+
