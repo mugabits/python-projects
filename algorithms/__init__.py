8f90fb636401dba46dd00afc88bb6a04c496a000
Jose Mugaburu <jose.mugaburu@gmail.com>
2015-10-25 18:36:31 -0400

    adds algorithms folder

---
+++ algorithms/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/ python
+
+"""
+	This section includes implementation of algorithms using Python
+"""
+
+
+__author__ = 'Jose Mugaburu'
+__maintainer__ = 'Jose Mugaburu'
+__email__ = 'mugab686@gmail.com'
+__status__ = 'Prototype'
