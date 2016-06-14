bd2e86731d76caf92860a92b54c2283547f8f980
Jose Mugaburu <jmugz3@users.noreply.github.com>
2016-06-14 19:08:35 -0400

    erases unnecessary comments

--- main.py
+++ main.py
@@ -25,13 +25,3 @@ def readGraphInput(filename):
 
     return graph_dict
 
-
-#
-# [(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]
-# [(1, 3), (1, 4), (2, 3), (2, 1), (2, 4), (3, 1), (3, 4)]
-#
-# >>> combs = []
-# >>> for x in [1,2,3]:
-# ...     for y in [3,1,4]:
-# ...         if x != y:
-# ...             combs.append((x, y))
